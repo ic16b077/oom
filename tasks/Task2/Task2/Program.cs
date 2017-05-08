4c0b217ad7154ccde4138d94cc9ce5f0ee1c89f3
Michael Käfer <ic16b077@technikum-wien.at>
2017-05-08 22:35:08 +0200

    add Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task2
+{
+
+	class Flugzeug
+	{
+		private int m_sitzplaetze;
+		private int m_fluegel;
+
+		public Flugzeug(int newFluegel, int newSitzplaetze)
+		{
+			this.Sitzplaetze = newSitzplaetze;
+			this.Fluegel = newFluegel;
+		}
+
+		public int SitzplaetzeMinusTwo()
+		{
+			return m_sitzplaetze * 2;
+		}
+
+		public int Fluegel
+		{
+			get
+			{
+				return m_fluegel;
+			}
+			set
+			{
+				if (value < 2) throw new Exception("Mindestens zwei Flügel angeben.");
+				m_fluegel = value;
+			}
+		}
+
+		public int Sitzplaetze
+		{
+			get
+			{
+				return m_sitzplaetze;
+			}
+			set
+			{
+				if (value < 2) throw new Exception("Mindestens zwei Sitzplätze angeben.");
+				m_sitzplaetze = value;
+			}
+		}
+	}
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Flugzeug a = new Flugzeug (2, 134);
+			Console.WriteLine (a.SitzplaetzeMinusTwo());
+		}
+	}
+}
