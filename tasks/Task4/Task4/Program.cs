e7413866f22e06f305761a61ff1dc6e20dfeda02
Michael Käfer <ic16b077@technikum-wien.at>
2017-05-19 18:57:25 +0200

    task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task4
+{
+	class Auto
+	{
+
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
