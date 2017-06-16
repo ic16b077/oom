598a74e44db5750c9f0a7bed80a16d414f9c9973
Michael Kaefer <ic16b077@TECHNIKUM-WIEN.AT>
2017-06-16 14:08:43 +0200

    bla

--- tasks/Task4/Task4/Program.cs
+++ tasks/Task4/Task4/Program.cs
@@ -11,16 +11,16 @@ namespace Task4
 	{
 		public static void Main (string[] args)
 		{
-			/* Inventarliste erzeugen */
-			var inventarliste = new Inventar[] {
-				new Zigarettenautomat(3153, new DateTime(2014, 5, 30), "Tschick-V-15", 412, true),
-				new Zigarettenautomat (4965, new DateTime (2008, 10, 3), "Smoke Now Z-31", 301, true),
-				new Fahrscheinautomat(1234, new DateTime(2010, 12, 5), "OEBB Fast and the Furious-Ticket", 123),
-				new Fahrscheinautomat(7234, new DateTime(2003, 2, 15), "DB Slow Motion Fahrkarte", 456),
-			};
+            /* Inventarliste erzeugen */
+            var inventarliste = new Inventar[] {
+                new Zigarettenautomat(3153, new DateTime(2014, 5, 30), "Tschick-V-15", 412, true),
+                new Zigarettenautomat(4965, new DateTime (2008, 10, 3), "Smoke Now Z-31", 301, true),
+                new Fahrscheinautomat(1234, new DateTime(2010, 12, 5), "OEBB Fast and the Furious-Ticket", 123),
+                new Fahrscheinautomat(7234, new DateTime(2003, 2, 15), "DB Slow Motion Fahrkarte", 456),
+            };
 
-			/* Inventarliste in JSON serialisieren */
-			string output = JsonConvert.SerializeObject (inventarliste);
+            /* Inventarliste in JSON serialisieren */
+            string output = JsonConvert.SerializeObject (inventarliste);
 
 			/* JSON in Datei schreiben */
 			string filename = @"M:\Inventar.json";
@@ -39,7 +39,7 @@ namespace Task4
 			Console.WriteLine(JsonConvert.SerializeObject(inventarlisteAusDatei, Formatting.Indented));
 
             /* T6.1 */
-            SimulateProducer.Run();
+            SimulateProducer.Run(inventarliste);
 		}
 	}
 }
--- tasks/Task4/Task4/SimulateProducer.cs
+++ tasks/Task4/Task4/SimulateProducer.cs
@@ -4,15 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reactive.Subjects;
+using System.Drawing;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Console;
 
 namespace Task4
 {
     public static class SimulateProducer
     {
-        public static void Run()
+        public static void Run(Inventar []inventarliste)
         {
-            var source = new Subject<Zigarettenautomat>();
-            Console.WriteLine("bla");
+            var producer = new Subject<Inventar>();
+
+            producer
+                .Where(x => x.Inventarnummer > 2000)
+                .Subscribe(x => Console.WriteLine($"Inventarnummer {x.Inventarnummer}, Modell: {x.Modell}"));
+
+            producer
+                .Where(x => x.Inventarnummer > 2000)
+                .Select(x => x.Ankaufdatum)
+                .Subscribe(x => Console.WriteLine($"Ankaufdatum: {x}\n"));
+
+            producer
+                .Skip(2)
+                .Take(1)
+                .Subscribe(x => Console.WriteLine($"Übersprungenes Inventar: {x}\n"));
+
+            foreach (var inventar in inventarliste)
+            {
+                Console.WriteLine("Warte auf Daten..");
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                producer.OnNext(inventar); // push value i to subscribers
+            }
         }
     }
 }
