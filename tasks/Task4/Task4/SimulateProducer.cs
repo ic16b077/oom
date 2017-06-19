4bf887fd2e228c0552f0cf1045a8f0c4cc9e2c56
ic16b077 <michael.kaefer@technikum-wien.at>
2017-06-19 21:27:21 +0200

    Update SimulateProducer.cs

--- tasks/Task4/Task4/SimulateProducer.cs
+++ tasks/Task4/Task4/SimulateProducer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,8 +41,3 @@ namespace Task4
         }
     }
 }
-/*
-Create a Subject<T> where T is one of your classes (e.g. Book) to simulate an external system pushing new T objects into your system.
-Experiment with different Rx queries.
-Also see http://rxwiki.wikidot.com/101samples.
-*/
