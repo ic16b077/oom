b5c17c565871c6ea91f2ce60bf8a25189499b48f
ic16b077 <michael.kaefer@technikum-wien.at>
2017-06-19 20:56:26 +0200

    Update Program.cs

--- tasks/Task4/Task4/Program.cs
+++ tasks/Task4/Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Newtonsoft.Json;
 using static System.Console;
 using System.Collections.Generic;
@@ -11,16 +11,16 @@ namespace Task4
 	{
 		public static void Main (string[] args)
 		{
-            /* Inventarliste erzeugen */
-            var inventarliste = new Inventar[] {
-                new Zigarettenautomat(3153, new DateTime(2014, 5, 30), "Tschick-V-15", 412, true),
-                new Zigarettenautomat(4965, new DateTime (2008, 10, 3), "Smoke Now Z-31", 301, true),
-                new Fahrscheinautomat(1234, new DateTime(2010, 12, 5), "OEBB Fast and the Furious-Ticket", 123),
-                new Fahrscheinautomat(7234, new DateTime(2003, 2, 15), "DB Slow Motion Fahrkarte", 456),
-            };
+            		/* Inventarliste erzeugen */
+		        var inventarliste = new Inventar[] {
+                		new Zigarettenautomat(3153, new DateTime(2014, 5, 30), "Tschick-V-15", 412, true),
+                		new Zigarettenautomat(4965, new DateTime (2008, 10, 3), "Smoke Now Z-31", 301, true),
+                		new Fahrscheinautomat(1234, new DateTime(2010, 12, 5), "OEBB Fast and the Furious-Ticket", 123),
+                		new Fahrscheinautomat(7234, new DateTime(2003, 2, 15), "DB Slow Motion Fahrkarte", 456),
+                	};
 
-            /* Inventarliste in JSON serialisieren */
-            string output = JsonConvert.SerializeObject (inventarliste);
+	    	        /* Inventarliste in JSON serialisieren */
+        		string output = JsonConvert.SerializeObject (inventarliste);
 
 			/* JSON in Datei schreiben */
 			string filename = @"M:\Inventar.json";
@@ -38,8 +38,10 @@ namespace Task4
 			/* Auf Console schreiben */
 			Console.WriteLine(JsonConvert.SerializeObject(inventarlisteAusDatei, Formatting.Indented));
 
-            /* T6.1 */
-            SimulateProducer.Run(inventarliste);
+            		/* T6.1 */
+            		SimulateProducer.Run(inventarliste);
+			/* T6.2 */
+			PlayWithTasks.Run(inventarliste);
 		}
 	}
 }
